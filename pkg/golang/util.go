90a2ab11be4e7c25f5c47116e7cd1dc962f5a74e
zhangjun.zoumo <jim.zoumo@gmail.com>
2021-08-06 14:22:30 +0800

    fix: get correct go version from command

--- pkg/golang/util.go
+++ pkg/golang/util.go
@@ -2,18 +2,37 @@ package golang
 
 import (
 	"fmt"
-	"runtime"
 	"strings"
 
 	"github.com/Masterminds/semver/v3"
+
+	"github.com/zoumo/make-rules/pkg/runner"
 )
 
 const (
 	MinimumGoVersion = "1.13.0"
 )
 
+func getGoVersion() (string, error) {
+	cmd := runner.NewRunner("go")
+	out, err := cmd.RunCombinedOutput("version")
+	if err != nil {
+		return "", err
+	}
+	output := strings.Split(string(out), " ")
+
+	if len(output) != 4 {
+		return "", fmt.Errorf("get error go version output: %v", string(out))
+	}
+	return output[2], nil
+}
+
 func VerifyGoVersion(minimumGoVersion string) error {
-	v := strings.TrimPrefix(runtime.Version(), "go")
+	version, err := getGoVersion()
+	if err != nil {
+		return err
+	}
+	v := strings.TrimPrefix(version, "go")
 	sv1, err := semver.NewVersion(v)
 	if err != nil {
 		return err
